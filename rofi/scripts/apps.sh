b4f1b13e7c0728e2bd759aed3175388827e7fd5f
orlando <or.diaz@duocuc.cl>
2024-10-24 18:45:46 -0300

    rofi

---
+++ rofi/scripts/apps.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Lanzar Rofi para seleccionar aplicaciones
+rofi -show drun -modi drun -theme-str 'element {
+    padding: 10px;
+    border: 2px solid;
+    border-radius: 5px;
+}' -font "monospace 10"
+
