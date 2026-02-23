c339a7315468ea850bdd9052f3a3a182403cb274
Orlando <169166413+OrlandoDiaz2003@users.noreply.github.com>
2026-02-22 23:41:33 -0300

    editor config

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,32 +1,35 @@
 -- OPTIONS
-vim.opt.number         = true
-vim.opt.relativenumber = true
-vim.opt.wrap           = false
-vim.opt.expandtab      = true
-vim.opt.list           = true
-vim.opt.pumheight      = 10
-vim.opt.virtualedit    = ""
-vim.opt.cmdheight      = 0
-vim.opt.tabstop        = 4
-vim.opt.swapfile       = false
-vim.opt.shiftwidth     = 4
-vim.opt.smartindent    = true
-vim.opt.smartcase      = true
-vim.opt.hlsearch       = false
-vim.o.listchars        = 'tab:→ ,lead:·,trail:·,space:·'
-vim.opt.incsearch      = true
-vim.opt.cursorline     = false
-vim.opt.showmode       = true
-vim.opt.termguicolors  = true
-vim.opt.scrolloff      = 12
-vim.opt.guicursor      = ""
-vim.opt.colorcolumn    = "80"
-vim.opt.winborder      = "none"
-vim.opt.laststatus     = 2
-vim.g.mapleader        = " "
-
+vim.o.number         = true
+vim.o.relativenumber = true
+vim.o.wrap           = false
+vim.o.expandtab      = true
+vim.o.list           = false
+vim.o.pumheight      = 10
+vim.o.virtualedit    = ""
+vim.o.cmdheight      = 0
+vim.o.tabstop        = 4
+vim.o.swapfile       = false
+vim.o.shiftwidth     = 4
+vim.o.smartindent    = true
+vim.o.smartcase      = true
+vim.o.hlsearch       = false
+vim.o.listchars      = 'tab:→ ,lead:·,trail:·,space:·'
+vim.o.guifont        = "Cascadia Mono:14"
+vim.o.incsearch      = true
+vim.o.cursorline     = false
+vim.o.showmode       = true
+vim.o.termguicolors  = true
+vim.o.scrolloff      = 12
+vim.o.guicursor      = ""
+vim.o.colorcolumn    = "80"
+vim.o.winborder      = "none"
+vim.o.laststatus     = 1
+vim.o.lazyredraw     = true
+vim.o.ttimeoutlen    = 10
+vim.g.mapleader      = " "
 --REMAPS
-vim.keymap.set("n", "<Leader>e", "<cmd>Oil<CR>")
+vim.keymap.set("n", "<Leader>e", "<cmd>Neotree toggle dir=./ reveal<CR>")
+vim.keymap.set("n", "<Leader>cd", ":cd %:p:h<CR>:pwd<CR>")
 vim.keymap.set("n", "<Leader>w", "<cmd>write<CR>")
 vim.keymap.set("n", "<Leader>q", ":quit<CR>")
 
@@ -42,163 +45,162 @@ vim.keymap.set("n", "<Leader>t",  ":vsplit | wincmd l | term<CR>")
 vim.keymap.set({ "n", "v", "x" }, "<Leader>y", '"+y <CR>')
 
 vim.keymap.set("n", "<Leader>f", vim.lsp.buf.format)
-
---Netrw config
-vim.g.netrw_liststyle = 1
-vim.g.netrw_banner = 0
-vim.g.netrw_sizestyle = "H"
-vim.g.netrw_timefmt = "%d %b %Y %H:%M"
-vim.g.netrw_keepdir = 0
-
-vim.api.nvim_create_autocmd('FileType', {
-	pattern = "netrw",
-	callback = function()
-		vim.wo.relativenumber = true
-		vim.wo.number = true
-	end,
-
-})
-
 --Plugins
 vim.pack.add({
-	{ src = "https://github.com/folke/tokyonight.nvim"},
-	{ src = "https://github.com/joshdick/onedark.vim"},
-	{ src = "https://github.com/oskarnurm/koda.nvim"},
-	{ src = "https://github.com/sphamba/smear-cursor.nvim"},
-	{ src = "https://github.com/stevearc/oil.nvim" },
-	{ src = "https://github.com/nvim-telescope/telescope.nvim" },
-	{ src = "https://github.com/nvim-lua/plenary.nvim" },
-	{ src = "https://github.com/nvim-treesitter/nvim-treesitter", version = "master", },
-	{ src = "https://github.com/rafamadriz/friendly-snippets" },
-	{ src = "https://github.com/Saghen/blink.cmp",                opts = {}, },
-	{ src = "https://github.com/neovim/nvim-lspconfig",           deps = { "blink.cmp" }, },
-	{ src = "https://github.com/mason-org/mason.nvim" },
-	{ src = "https://github.com/mason-org/mason-lspconfig.nvim" },
-	{ src = "https://github.com/L3MON4D3/LuaSnip" },
-	{ src = "https://github.com/ej-shafran/compile-mode.nvim" ,deps={'plenary'} },
-	{ src = "https://github.com/windwp/nvim-ts-autotag", build = "make install_jsregexp", },
+    { src = "https://github.com/folke/tokyonight.nvim"},
+    { src = "https://github.com/rebelot/kanagawa.nvim"},
+    { src = "https://github.com/nvim-neo-tree/neo-tree.nvim"},
+    { src = "https://github.com/romgrk/barbar.nvim"},
+    { src = "https://github.com/nvim-telescope/telescope.nvim" },
+
+    --Dependencies
+    "https://github.com/MunifTanjim/nui.nvim",
+    "https://github.com/nvim-tree/nvim-web-devicons",
+
+    { src = "https://github.com/windwp/nvim-autopairs" },
+    { src = "https://github.com/nvim-lua/plenary.nvim" },
+    { src = "https://github.com/nvim-treesitter/nvim-treesitter", version = "master", },
+    { src = "https://github.com/rafamadriz/friendly-snippets" },
+    { src = "https://github.com/Saghen/blink.cmp",                opts = {}, },
+    { src = "https://github.com/neovim/nvim-lspconfig",           deps = { "blink.cmp" }, },
+    { src = "https://github.com/mason-org/mason.nvim" },
+    { src = "https://github.com/mason-org/mason-lspconfig.nvim" },
+    { src = "https://github.com/L3MON4D3/LuaSnip" },
+    { src = "https://github.com/ej-shafran/compile-mode.nvim" ,deps={'plenary'} },
+    { src = "https://github.com/windwp/nvim-ts-autotag", build = "make install_jsregexp", },
 })
 
 vim.g.compile_mode = {}
 
-vim.cmd("color tokyonight-night")
+vim.cmd("color kanagawa")
+
+vim.g.barbar_auto_setup = false
+require 'barbar'.setup {
+    auto_hide = true
+}
+local map = vim.api.nvim_set_keymap
+local opts = { noremap = true, silent = true }
+
+map('n', '<A-1>', '<Cmd>BufferGoto 1<CR>', opts)
+map('n', '<A-2>', '<Cmd>BufferGoto 2<CR>', opts)
+map('n', '<A-3>', '<Cmd>BufferGoto 3<CR>', opts)
+map('n', '<A-4>', '<Cmd>BufferGoto 4<CR>', opts)
+map('n', '<A-5>', '<Cmd>BufferGoto 5<CR>', opts)
+map('n', '<A-6>', '<Cmd>BufferGoto 6<CR>', opts)
+map('n', '<A-7>', '<Cmd>BufferGoto 7<CR>', opts)
+map('n', '<A-8>', '<Cmd>BufferGoto 8<CR>', opts)
+map('n', '<A-9>', '<Cmd>BufferGoto 9<CR>', opts)
+map('n', '<A-0>', '<Cmd>BufferLast<CR>', opts)
+map('n', '<A-c>', '<Cmd>BufferClose<CR>', opts)
+
 
 --Plugin config
-require"smear_cursor".enabled = true
-
-require "oil".setup({
-	columns = {
-		"permissions",
-		"size",
-		"mtime"
-	},
-	view_options = {
-		show_hidden = true,
-	}
+require "nvim-autopairs".setup({
+    event = "InsertEnter",
+    config = true
 })
 
-require("nvim-treesitter.configs").setup({
-	ensure_installed = {
-		'c',
-		'lua',
-		'html',
-		'css',
-		'python',
-		'javascript',
-	},
-	sync_install = false,
-	auto_install = true,
 
-	highlight = {
-		enable =  true,
-		additional_vim_regex_highlighting = false,
-	},
-	indent = {
-		enable = true,
-	},
+require("nvim-treesitter.configs").setup({
+    ensure_installed = {
+        'c',
+        'lua',
+        'html',
+        'css',
+        'python',
+        'javascript',
+    },
+    sync_install = false,
+    auto_install = true,
+
+    highlight = {
+        enable =  true,
+        additional_vim_regex_highlighting = false,
+    },
+    indent = {
+        enable = true,
+    },
+    auto_pairs = {
+        enable = true
+    }
 })
 
 vim.api.nvim_create_autocmd('PackChanged', {
-	desc = 'Handle nvim-treesitter updates',
-	group = vim.api.nvim_create_augroup('nvim-treesitter-pack-changed-update-handler', { clear = true }),
-	callback = function(event)
-		if event.data.kind == 'update' then
-			vim.notify('nvim-treesitter updated, running TSUpdate...', vim.log.levels.INFO)
-			---@diagnostic disable-next-line: param-type-mismatch
-			local ok = pcall(vim.cmd, 'TSUpdate')
-			if ok then
-				vim.notify('TSUpdate completed successfully!', vim.log.levels.INFO)
-			else
-				vim.notify('TSUpdate command not available yet, skipping', vim.log.levels.WARN)
-			end
-		end
-	end,
+    desc = 'Handle nvim-treesitter updates',
+    group = vim.api.nvim_create_augroup('nvim-treesitter-pack-changed-update-handler', { clear = true }),
+    callback = function(event)
+        if event.data.kind == 'update' then
+            vim.notify('nvim-treesitter updated, running TSUpdate...', vim.log.levels.INFO)
+            ---@diagnostic disable-next-line: param-type-mismatch
+            local ok = pcall(vim.cmd, 'TSUpdate')
+            if ok then
+                vim.notify('TSUpdate completed successfully!', vim.log.levels.INFO)
+            else
+                vim.notify('TSUpdate command not available yet, skipping', vim.log.levels.WARN)
+            end
+        end
+    end,
 })
 
-local themes = require('telescope.themes')
 local builtin = require('telescope.builtin')
-local ivy_opts = themes.get_ivy()
-
-vim.keymap.set('n', '<leader><space>', function()
-  builtin.find_files(ivy_opts)
-end, { desc = 'Telescope find files (ivy)' })
-vim.keymap.set('n', '<leader>fg', function()
-  builtin.live_grep(ivy_opts)
-end, { desc = 'Telescope live grep (ivy)' })
-vim.keymap.set('n', '<leader>fb', function()
-  builtin.buffers(ivy_opts)
-end, { desc = 'Telescope buffers (ivy)' })
-vim.keymap.set('n', '<leader>fh', function()
-  builtin.help_tags(ivy_opts)
-end, { desc = 'Telescope help tags (ivy)' })
+vim.keymap.set('n', '<leader><space>', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg',      builtin.live_grep , { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fb',      builtin.buffers   , { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fh',      builtin.help_tags , { desc = 'Telescope help tags' })
 
 require("mason").setup()
 require("mason-lspconfig").setup {
-	ensure_installed = {
-		"clangd", "bashls", "html", "emmet_ls"
-	}
+    ensure_installed = {
+        "clangd", "bashls", "html", "emmet_ls"
+    }
 }
 local capabilities = require('blink.cmp').get_lsp_capabilities()
 local servers = { "clangd", "pyright", "bashls", "html", "emmet_ls", "ts_ls", "eslint" }
 
 for _, server in ipairs(servers) do
-	vim.lsp.config(server,{ capabilities = capabilities })
+    vim.lsp.config(server,{ capabilities = capabilities })
 end
 
 require("blink.cmp").setup({
-	keymap = { preset = "default" },
-	appearance = { nerd_font_variant = 'mono' },
-	completion = { documentation = { auto_show = false } },
-	sources = {
-		default = { 'lsp', 'path', 'snippets', 'buffer' },
-	},
-	fuzzy = { implementation = "lua" }
+    keymap = { preset = "default" },
+    appearance = { nerd_font_variant = 'mono' },
+    completion = { documentation = { auto_show = false } },
+    sources = {
+        default = { 'lsp', 'path', 'snippets', 'buffer' },
+    },
+    fuzzy = { implementation = "lua" }
 })
 
 vim.diagnostic.config({
-	virtual_text = true,
-	signs = true,
-	underline = true,
-	update_in_insert = false,
-	severity_sort = true,
+    virtual_text = false,
+    signs = true,
+    underline = true,
+    update_in_insert = false,
+    severity_sort = true,
 })
 
 require('nvim-ts-autotag').setup({})
-vim.lsp.handlers['textDocument/publishDiagnostics'] = vim.lsp.with(
-	vim.lsp.diagnostic.on_publish_diagnostics,
-	{
-		underline = true,
-		virtual_text = {
-			spacing = 5,
-			severity_limit = 'Warning',
-		},
-		update_in_insert = true,
-	}
-)
 vim.api.nvim_create_autocmd('TextYankPost', {
-	desc = 'Highlight when yanking (copying) text',
-	group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-	callback = function()
-		vim.hl.on_yank()
-	end,
+    desc = 'Highlight when yanking (copying) text',
+    group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+    callback = function()
+        vim.hl.on_yank()
+    end,
+})
+
+vim.api.nvim_create_autocmd('LspAttach', {
+    desc = 'LSP actions',
+    callback = function(event)
+        local opts = {buffer = event.buf}
+        vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
+        vim.keymap.set('n', '<leader>k', '<cmd>lua vim.diagnostic.open_float()<cr>', opts)
+        vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
+        vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
+        vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
+        vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
+        vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
+        vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
+        vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
+        vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
+    end,
 })
